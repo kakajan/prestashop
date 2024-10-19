63dd986e1afd4d178eec13268ce0dce21b0137c2
Usher <faslolkhitab@gmail.com>
2024-10-19 16:47:53 +0330

    updates

--- homecategories/controllers/admin/AdminHomeCategoriesController.php
+++ homecategories/controllers/admin/AdminHomeCategoriesController.php
@@ -26,55 +26,54 @@ class AdminHomeCategoriesController extends ModuleAdminController
         );
     }
 
-public function initContent()
-{
-    parent::initContent();
-    $this->setTemplate('module:homecategories/views/templates/admin/configure.tpl');
-}
-
+    public function initContent()
+    {
+        parent::initContent();
+        $this->setTemplate('module:homecategories/views/templates/admin/configure.tpl');
+    }
 
     public function postProcess()
-{
-    if (Tools::isSubmit('submitHomeCategories')) {
-        $selectedCategories = Tools::getValue('CATEGORIES');
-        Configuration::updateValue('HOME_CATEGORIES', implode(',', $selectedCategories));
-        $this->confirmations[] = $this->l('Settings updated');
+    {
+        if (Tools::isSubmit('submitHomeCategories')) {
+            $selectedCategories = Tools::getValue('CATEGORIES');
+            Configuration::updateValue('HOME_CATEGORIES', implode(',', $selectedCategories));
+            $this->confirmations[] = $this->l('Settings updated');
+        }
     }
-}
 
-   public function renderForm()
-{
-    $categories = Category::getCategories($this->context->language->id, true);
-    $selectedCategories = explode(',', Configuration::get('HOME_CATEGORIES'));
+    public function renderForm()
+    {
+        $categories = Category::getCategories($this->context->language->id, true);
+        $selectedCategories = explode(',', Configuration::get('HOME_CATEGORIES'));
 
-    $this->fields_form = array(
-        'form' => array(
-            'legend' => array(
-                'title' => $this->l('Home Categories Settings'),
-            ),
-            'input' => array(
-                array(
-                    'type' => 'select',
-                    'label' => $this->l('Select Categories'),
-                    'name' => 'CATEGORIES[]',
-                    'multiple' => true,
-                    'options' => array(
-                        'query' => $categories,
-                        'id' => 'id_category',
-                        'name' => 'name',
+        $this->fields_form = array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Home Categories Settings'),
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'select',
+                        'label' => $this->l('Select Categories'),
+                        'name' => 'CATEGORIES[]',
+                        'multiple' => true,
+                        'options' => array(
+                            'query' => $categories,
+                            'id' => 'id_category',
+                            'name' => 'name',
+                        ),
+                        'default_value' => $selectedCategories,
+                        'desc' => $this->l('Select categories to display on the homepage.'),
                     ),
-                    'default_value' => $selectedCategories,
-                    'desc' => $this->l('Select categories to display on the homepage.')
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                    'name' => 'submitHomeCategories',
                 ),
             ),
-            'submit' => array(
-                'title' => $this->l('Save'),
-                'name' => 'submitHomeCategories'
-            ),
-        ),
-    );
+        );
 
-    return parent::renderForm();
-}
+        return parent::renderForm();
+    }
 
 }
--- homecategories/views/templates/hook/homecategories.tpl
+++ homecategories/views/templates/hook/homecategories.tpl
@@ -1,8 +1,9 @@
+<link rel="stylesheet" href="{$module_dir}views/css/homecategories.css">
 <div class="home-categories">
     {foreach from=$categories item=category}
         <div class="category">
             <a href="{$link->getCategoryLink($category.id_category, $category.link_rewrite)}">
-                <img src="{$link->getCatImageLink($category.link_rewrite, $category.id_image)}" alt="{$category.name}">
+                <img src="{$link->getBaseLink()}img/c/{$category.id_category}.jpg" alt="{$category.name}">
                 <h3>{$category.name}</h3>
             </a>
         </div>
